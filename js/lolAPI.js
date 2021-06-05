ddfa4428953566a606d0fb720fb9d1cf04ebb143
LeeSeokBong <kingstar6035@gmail.com>
2021-06-05 23:27:45 +0900

    롤 전적 검색 기본 완료

--- js/lolAPI.js
+++ js/lolAPI.js
@@ -1,77 +1,65 @@
 
-var api_key = 'RGAPI-39c81960-071d-4d9c-a9c8-4ef8be116037'
-var lolId = 'ndbg'
-console.log(api_key)
-var sohwan = 'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + lolId 
-console.log(sohwan)
-console.log(sohwan.id)
+var api_key = 'RGAPI-86039d0b-9734-40ca-8457-8171006e3ded'
+var summonerSearchBtn = document.querySelector("#summonerSearchBtn");
+summonerSearchBtn.addEventListener("click", function (){
+  var lolId = document.querySelector("#summoner").value
+  var url = 'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/' + lolId
+  $.ajax({
+    beforeSend : function(xhr){
+      xhr.setRequestHeader("X-Riot-Token", api_key);
+      xhr.setRequestHeader("Access-Control-Allow-Origin","https://developer.riotgames.com/");
+      xhr.setRequestHeader("Access-Control-Allow-Methods","GET");
+      xhr.setRequestHeader("Access-Control-Allow-Headers","origin, x-requested-with, content-type, accept");
+  },
+  type:"GET",
+      url: url,
+      jsonp:"callback",
+      data: {
+              "api_key": api_key
+          },
+      success: function(response) {
+        drawLolIdInfo(response)
+      }
+  });
+})
 
+function drawLolIdInfo(response) {
+  console.log(response)
+  var accountId = response.id
+  document.querySelector("#lolId").textContent = response.name;
+  document.querySelector("#lolLevel").textContent = response.summonerLevel;
+  leagueInfo(accountId)
+}
 
-// function ajax() {
-//   fetch("https://yts.mx/api/v2/list_movies.json").then(function(response) {
-//     console.log(response)
-//   })
-// }
-// ajax();
-// $.ajax({
-//   url: sohwan,
-//   type: 'GET',
-//   dataType: 'json',
-//   data: {
-//       "api_key": api_key,
-//   },
-//   success: function(res){
-//       console.log(res);
-//   },
-//   error: function(req, stat, err){
-//       console.log(err);
-//   },
-// });
-$.ajax({
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-"Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
-"Access-Control-Max-Age": 3600,
-"Access-Control-Allow-Headers": "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization",
-    "X-Riot-Token": api_key,
-    "Access-Control-Allow-Origin" : "*",
-  "Access-Control-Allow-Headers" : "Content-Type, Authorization",
-  "Access-Control-Max-Age": "3600",
-  "Access-Control-Allow-Headers": "x-requested-with"},
-    type:"GET",
-    url:sohwan,
-    dataType: 'json',
-    data: {
-            "api_key": api_key,
-        },
-    success: function(data) {
-      console.log(data)
-    }
-});
-
-// fetch(sohwan + "?api_key=" + api_key, {
-//     headers: { 
-//           "X-Riot-Token": api_key,
-//     "Access-Control-Allow-Origin" : "*",
-//   "Access-Control-Allow-Headers" : "Content-Type, Authorization",
-//   "Access-Control-Max-Age": "3600",
-//   "Access-Control-Allow-Headers": "x-requested-with"},
-//      }
-// )
-// .then(res => res.json())
-// .then(data => console.log(JSON.stringify(data, null, '\t')));
-
-
-// $.ajax({
-//     type: 'GET',
-//     crossDomain: true,
-//     dataType: 'jsonp',
-//     url: sohwan,
-//     success: function(jsondata){
-//         console.log(jsondata)
-//     }
-//  })
-    
-// fetch(sohwan)
-// .then(res => res.json())
-// .then(data => console.log(data));
+function leagueInfo(accountId) {
+  var leagueInfoUrl = 'https://kr.api.riotgames.com//lol/league/v4/entries/by-summoner/' + accountId
+  $.ajax({
+    beforeSend : function(xhr){
+      xhr.setRequestHeader("X-Riot-Token", api_key);
+      xhr.setRequestHeader("Access-Control-Allow-Origin","https://developer.riotgames.com/");
+      xhr.setRequestHeader("Access-Control-Allow-Methods","GET");
+      xhr.setRequestHeader("Access-Control-Allow-Headers","origin, x-requested-with, content-type, accept");
+  },
+  type:"GET",
+      url: leagueInfoUrl,
+      jsonp:"callback",
+      data: {
+              "api_key": api_key
+          },
+      success: function(response) {
+        drawleagueInfo(response)
+      }
+  });
+}
+function drawleagueInfo(response) {
+  console.log(response)
+  document.querySelector("#lolTier").textContent = response[0].tier
+  document.querySelector("#lolLank").textContent = response[0].lank
+  document.querySelector("#lolLeaguePoints").textContent = response[0].leaguePoints + "점"
+  var lolTotalRateSpan = document.querySelector("#lolTotalRate");
+  var lolTotalRate = response[0].wins + response[0].losses
+  lolTotalRateSpan.textContent = lolTotalRate + "전"
+  document.querySelector("#lolWins").textContent = response[0].wins + "승"
+  document.querySelector("#lolLosses").textContent = response[0].losses + "패"
+  document.querySelector("#lolWinningRate").textContent = (response[0].wins / lolTotalRate * 100).toFixed(2) + "%"
+}
